67d7a8632ca3226d980bd80004947f63d3576b48
Naveen0417 <naveenkumarreddy106@gmail.com>
2023-04-01 16:13:38 +0530

    Updated Runner file

--- src/test/java/com/Sample/runner/TestRunner.java
+++ src/test/java/com/Sample/runner/TestRunner.java
@@ -5,8 +5,8 @@ import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
     
-@CucumberOptions(tags = "@VeifyHomePage", features = {"src/test/resources/features"}, glue = {"com.Sample.stepdefinitions"},
-                 plugin = "pretty")
+@CucumberOptions(tags = "@VeifyHomePage", features = {"src/test/resources/features/"}, glue = {"com.Sample.stepdefinitions"},
+plugin = {"pretty", "json:target/CucumberTestReport.json", "html:target/CucumberTestReport.html"})
     
 public class TestRunner extends AbstractTestNGCucumberTests {
     
