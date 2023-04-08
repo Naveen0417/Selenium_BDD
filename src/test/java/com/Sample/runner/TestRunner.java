b59c1d516f4769a0cb2571b9169be5943d9d246c
Naveen0417 <naveenkumarreddy106@gmail.com>
2023-04-08 17:24:08 +0530

    Updated TestRunner

--- src/test/java/com/Sample/runner/TestRunner.java
+++ src/test/java/com/Sample/runner/TestRunner.java
@@ -5,7 +5,11 @@ import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
     
+<<<<<<< Updated upstream
 @CucumberOptions(tags = "@VeifyHomePage", features = {"src/test/resources/features/"}, glue = {"com.Sample.stepdefinitions"},
+=======
+@CucumberOptions(tags = "@Test", features = {"src/test/resources/features/"}, glue = {"com.Sample.stepdefinitions"},
+>>>>>>> Stashed changes
 plugin = {"pretty", "json:target/CucumberTestReport.json", "html:target/CucumberTestReport.html"})
     
 public class TestRunner extends AbstractTestNGCucumberTests {
