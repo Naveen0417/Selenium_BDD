1d6b9a2f6c57245b9bd36794caa0575e9c368235
z0hny <jjoshi8art@gmail.com>
2023-04-08 17:09:39 +0530

    US_HomePageUpdate_Test_v1

--- src/test/java/com/Sample/runner/TestRunner.java
+++ src/test/java/com/Sample/runner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
     
-@CucumberOptions(tags = "@VeifyHomePage", features = {"src/test/resources/features/"}, glue = {"com.Sample.stepdefinitions"},
+@CucumberOptions(tags = "@VeifyPIMtab", features = {"src/test/resources/features/"}, glue = {"com.Sample.stepdefinitions"},
 plugin = {"pretty", "json:target/CucumberTestReport.json", "html:target/CucumberTestReport.html"})
     
 public class TestRunner extends AbstractTestNGCucumberTests {
