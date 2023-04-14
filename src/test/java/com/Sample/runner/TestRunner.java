66048fc9ddd128126fad5f2f431cc959cc561fc1
Naveen0417 <naveenkumarreddy106@gmail.com>
2023-04-14 13:10:23 +0530

    Add Spark Report

--- src/test/java/com/Sample/runner/TestRunner.java
+++ src/test/java/com/Sample/runner/TestRunner.java
@@ -6,8 +6,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
     
 @CucumberOptions(tags = "@VeifyPIMtab", features = {"src/test/resources/features/"}, glue = {"com.Sample.stepdefinitions"},
-plugin = {"pretty", "json:target/CucumberTestReport.json", "html:target/CucumberTestReport.html"})
+plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
     
 public class TestRunner extends AbstractTestNGCucumberTests {
     
 }
+
+
+//"pretty", "json:target/CucumberTestReport.json", "html:target/CucumberTestReport.html"
